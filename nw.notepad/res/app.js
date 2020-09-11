c873a457060b0492b32c09ffdb1038c8de7a2964
张钰峰 <zyf47976@ly.com>
2020-09-11 09:25:58 +0800

    0.0.1

---
+++ nw.notepad/res/app.js
@@ -0,0 +1,324 @@
+void (function () {
+    let nWin = nw.Window.get()
+    let nWinMaxSize = [0, 0]
+    // 监听最小化事件
+    nWin.on("maximize", function () {
+        nWinMaxSize = [nWin.width, nWin.height]
+    })
+
+    WebEdit.renderTo("note-box")
+
+    let $ = function (cot) {
+        return typeof cot == "string" ? document.getElementById(cot) : cot
+    }
+
+    function htmlToText(str) {
+        return str.replace(/\</g, "&lt;").replace(/\>/g, "&gt;").replace(/"/g, "&#34;").replace(/'/g, "&#39;")
+    }
+
+    const fs = require("fs")
+    const path = require("path")
+
+    const rootDir = process.execPath.replace(/[^\/\\]*$/, path.join("nw.notepad", "data"))
+
+    function getPath(...arg) {
+        return path.resolve(...[rootDir].concat(arg))
+    }
+
+    function getUUID() {
+        let t = new Date().getTime() - 1599783690695
+        return parseInt(Math.floor(Math.random() * 100) + "" + t).toString(36)
+    }
+
+    let curBook = {}
+    // console.log("curBook", curBook)
+    let curNote = {}
+    let bookIds = []
+
+    function createBook(title) {
+        let id = getUUID()
+        fs.mkdirSync(getPath(id))
+        curBook = {
+            id,
+            title,
+            tree: [],
+            treeHash: {}
+        }
+
+        getBook()
+
+        return id
+    }
+
+    // 读取目录
+    function readBooks() {
+        // 读取目录中的所有文件/目录
+        let paths = fs.readdirSync(rootDir) || []
+        if (paths.length == 0) {
+            paths.push(createBook("默认笔记本"))
+        }
+
+        return paths
+    }
+
+    // 所有书本id
+    bookIds = readBooks()
+    // 当前书本
+    curBook = require(getPath(bookIds[0], "index.json")) || {}
+
+    function reSetLib() {
+        let libs = []
+        bookIds.forEach(function (id) {
+            let book = require(getPath(id, "index.json"))
+            libs.push('<div class="lib-li' + (id == curBook.id ? " act" : "") + '"><i class="iconfont l">&#xe7d4;</i><i class="iconfont r" event="removeBook:' + id + '">&#xe780;</i><b event="electBook:' + id + '">' + htmlToText(book.title) + "</b></div>")
+        })
+
+        $("lib.lis").innerHTML = libs.join("")
+    }
+
+    function reSetBookTree() {
+        let treeText = []
+        curBook.tree.forEach(function (id) {
+            let li = curBook.treeHash[id]
+            treeText.push('<div class="li' + (curNote.id == li.id ? " act" : "") + '" id="book.tree.li.' + li.id + '"><div class="con" event="electNote:' + li.id + '"><i class="r iconfont" event="removeNote:' + li.id + '">&#xe780;</i><i class="x iconfont">&#xe7d4;</i><b id="book.tree.title.' + li.id + '">' + htmlToText(li.title) + "</b></div></div>")
+        })
+
+        $("book.tree").innerHTML = treeText.join("")
+    }
+
+    function getBook(id) {
+        if (id && id != curBook.id) {
+            curBook = require(getPath(id, "index.json"))
+        }
+
+        $("book.title").value = curBook.title
+        if (curBook.tree.length == 0) {
+            createNote("新建笔记")
+        }
+
+        reSetBookTree()
+        electNote(curBook.tree[0])
+    }
+
+    function removeBookDir(dir) {
+        if (fs.existsSync(dir)) {
+            files = fs.readdirSync(dir) || []
+            files.forEach(function (file) {
+                let curPath = path.join(dir, file)
+                if (fs.statSync(curPath).isDirectory()) {
+                    // recurse
+                    removeBookDir(curPath)
+                } else {
+                    // delete file
+                    fs.unlinkSync(curPath)
+                }
+            })
+            fs.rmdirSync(dir)
+        }
+    }
+
+    function removeBook(id) {
+        let book = require(getPath(id, "index.json"))
+        let msg = ["确认删除《" + book.title + "》"]
+        msg.push(bookIds.length > 1 ? "删除后，无法恢复？" : "删除后，将会重新初始化？")
+        if (book && window.confirm(msg.join("\n"))) {
+            bookIds.splice(bookIds.indexOf(book.id), 1)
+
+            removeBookDir(getPath(book.id))
+
+            if (bookIds.length == 0) {
+                bookIds.push(createBook("默认笔记本"))
+            }
+
+            if (book.id == curBook.id) {
+                curBook = {}
+                getBook(bookIds[0])
+            }
+        }
+    }
+
+    function saveNote() {
+        let bookId = curBook.id
+        let noteId = curNote.id
+        if (bookId && noteId) {
+            fs.writeFileSync(getPath(bookId, noteId + ".html"), WebEdit.getHTML(), "utf8")
+        }
+    }
+
+    function getNote() {
+        try {
+            return fs.readFileSync(getPath(curBook.id, curNote.id + ".html"), "utf8")
+        } catch (e) {}
+        return ""
+    }
+
+    function saveCurBook() {
+        fs.writeFileSync(getPath(curBook.id, "index.json"), JSON.stringify(curBook), "utf8")
+    }
+
+    function createNote(title) {
+        let id = getUUID()
+        curBook.tree.push(id)
+        curBook.treeHash[id] = {
+            id,
+            title
+        }
+        saveCurBook()
+        reSetBookTree()
+        electNote(id)
+        WebEdit.setHTML("")
+        saveNote()
+        return id
+    }
+
+    function electNote(id) {
+        let cur = $("book.tree.li." + curNote.id)
+        if (cur) {
+            cur.className = "li"
+        }
+        if (id && id != curNote.id) {
+            curNote = curBook.treeHash[id]
+        }
+        cur = $("book.tree.li." + curNote.id)
+        if (cur) {
+            cur.className = "li act"
+        }
+        // console.log("curNote", curNote, id)
+        $("note.title").value = curNote.title
+        WebEdit.setHTML(getNote())
+    }
+
+    let ppEventFn = {
+        electNote(event, id) {
+            // console.log("event", event)
+            saveNote()
+            electNote(id)
+        },
+        addNote() {
+            createNote("新建笔记")
+            $("note.title").select()
+        },
+        removeNote(event, id) {
+            let note = curBook.treeHash[id]
+            if (note) {
+                if (curBook.tree.length == 1) {
+                    if (window.confirm("无法删除最后一遍笔记，是否删除整个笔记本？")) {
+                        removeBook(curBook.id)
+                    }
+                    return true
+                }
+
+                if (window.confirm("确认删除《" + note.title + "》?")) {
+                    delete curBook.treeHash[id]
+                    let index = curBook.tree.indexOf(id)
+                    curBook.tree.splice(index, 1)
+                    try {
+                        fs.unlinkSync(getPath(curBook.id, id + ".html"))
+                    } catch (e) {}
+
+                    reSetBookTree()
+
+                    if (curNote.id == id) {
+                        electNote(curBook.tree[0])
+                    }
+                }
+            }
+
+            return true
+        },
+        stop(event) {
+            event.stopPropagation()
+            return true
+        },
+        // 显示
+        showLibs() {
+            reSetLib()
+            $("lib.block").className = "lib-block act"
+        },
+        hideLibs() {
+            $("lib.block").className = "lib-block"
+        },
+        addBook() {
+            bookIds.push(createBook("新建笔记本"))
+            ppEventFn.hideLibs()
+
+            $("book.title").select()
+        },
+        electBook(event, id) {
+            getBook(id)
+            ppEventFn.hideLibs()
+        },
+        removeBook(event, id) {
+            removeBook(id)
+            ppEventFn.hideLibs()
+            return true
+        },
+        winClose() {
+            nWin.close()
+        },
+        winMinimize() {
+            nWin.minimize()
+        },
+        winToggle() {
+            if (nWin.width == nWinMaxSize[0] && nWin.height == nWinMaxSize[1]) {
+                nWin.unmaximize()
+            } else {
+                nWin.maximize()
+            }
+        }
+    }
+
+    function ppEvent(event) {
+        let target = event.target
+        while (target && target != this) {
+            if (target.getAttribute) {
+                let evn = target.getAttribute("event")
+                if (evn) {
+                    let args = evn.split(":")
+                    let fnKey = args[0]
+                    args[0] = event
+                    let flag = ppEventFn[fnKey].apply(this, args)
+                    if (flag) {
+                        break
+                    }
+                }
+            }
+            target = target.parentNode
+        }
+    }
+    $("context").onclick = ppEvent
+
+    getBook()
+
+    function keyDownEventFn(event) {
+        let key = event.key.toLowerCase()
+        let ctrl = event.ctrlKey
+        if (key == "f5") {
+            window.location.reload()
+            return
+        }
+        if (key == "s" && ctrl) {
+            // 保存
+            saveNote()
+            return
+        }
+    }
+    document.onkeydown = keyDownEventFn
+
+    WebEdit.iframeWin.addEventListener("keydown", keyDownEventFn, false)
+
+    // 标题更新
+    $("note.title").onblur = function () {
+        let val = this.value.trim()
+        $("book.tree.title." + curNote.id).innerText = val
+        curBook.treeHash[curNote.id].title = val
+
+        saveCurBook()
+    }
+
+    // 更新book名称
+    $("book.title").onblur = function () {
+        curBook.title = this.value.trim()
+        saveCurBook()
+    }
+})()
---
+++ nw.notepad/res/edit.js
@@ -0,0 +1,171 @@
+void (function () {
+    let WebEdit = (window.WebEdit = {})
+    let $ = function (cot) {
+        return typeof cot == "string" ? document.getElementById(cot) : cot
+    }
+
+    function getColors(format, jStart) {
+        jStart = jStart || 0
+
+        let v = [],
+            i,
+            j,
+            k,
+            c,
+            is,
+            js,
+            ks,
+            to16 = "0123456789ABCDEF"
+        for (i = 0; i <= 15; i += 3) {
+            is = to16.charAt(i) + to16.charAt(i)
+            for (j = jStart; j <= jStart + 6; j += 3) {
+                js = to16.charAt(j) + to16.charAt(j)
+                for (k = 0; k <= 15; k += 3) {
+                    ks = to16.charAt(k) + to16.charAt(k)
+                    c = "#" + js + ks + is
+                    v.push('<a href="javascript:;" title="' + c + '" v-click="format,' + format + "," + c + '" style="background-color:' + c + '"></a>')
+                }
+            }
+            v.push("\n")
+        }
+        if (j < 15) {
+            v.push(getColors(format, 9))
+        }
+        return v.join("")
+    }
+
+    function callFuns() {
+        let key = Array.prototype.shift.call(arguments)
+        WebEdit[key].apply(WebEdit, arguments)
+    }
+
+    function bind(fn, self, ...args) {
+        return function (...arg) {
+            fn.apply(self || this, args.concat(arg))
+        }
+    }
+
+    Object.assign(WebEdit, {
+        defDeploys: ["fontsize", "Bold", "Italic", "Underline", "Justifyleft", "Justifycenter", "Justifyright", "Insertorderedlist", "Insertunorderedlist", "Outdent", "Indent", "foreColor", "backColor", "|"],
+        renderTo: function (cot, deploys) {
+            this.deploys = deploys || this.defDeploys
+            let htmls = ['<table cellpadding="0" cellspacing="0" width="100%" class="WebEdit_Edit">', "<thead>", "<tr>", '<td nowrap="nowrap"><div class="WebEdit_empty1"></div>']
+            for (let i = 0, t; i < this.deploys.length; i += 1) {
+                t =
+                    this.deploys[i] == "-"
+                        ? [this.htmlDeploys[this.deploys[i]]]
+                        : [
+                              '<div class="WebEdit_left">', // v-click="WebEdit.stopClick(event);"
+                              this.htmlDeploys[this.deploys[i]],
+                              "</div>"
+                          ]
+                htmls.push(t.join(""))
+            }
+            htmls.push(["</td>", "</tr>", "</thead>", "<tbody>", "<tr>", "<td>", '<iframe width="100%" name="WebEdit_HTMLEdit" id="WebEdit_HTMLEdit" class="WebEdit_HTMLEdit" frameBorder="0" marginHeight="15" marginWidth="15"></iframe>', "</td>", "</tr>", "</tbody>", "</table>"].join(""))
+            cot = $(cot)
+            cot.innerHTML = htmls.join("")
+            let tags = cot.getElementsByTagName("*")
+            for (let i = 0, tag, v_click, v_over, v_leave; i < tags.length; i += 1) {
+                tag = tags[i]
+                v_click = tag.getAttribute("v-click")
+                if (v_click) {
+                    //                    console.log(v_click);
+                    tag.onclick = bind.apply(window, [callFuns, null].concat(v_click.split(",")))
+                }
+                v_over = tag.getAttribute("v-mouseover")
+                if (v_over) {
+                    //                    console.log(v_over);
+                    tag.onmouseover = bind.apply(window, [callFuns, null].concat(v_over.split(",")))
+                }
+                v_leave = tag.getAttribute("v-mouseleave")
+                if (v_leave) {
+                    tag.onmouseleave = bind.apply(window, [callFuns, null].concat(v_leave.split(",")))
+                }
+            }
+
+            this.iframe = window.frames["WebEdit_HTMLEdit"]
+            this.iframe.document.designMode = "on"
+            this.iframeWin = $("WebEdit_HTMLEdit").contentWindow
+            //window obblur
+            //FF
+            this.iframeWin.addEventListener("click", WebEdit.close, false)
+        },
+        showCot: null,
+        show: function (id) {
+            let c = $("WebEdit.Down." + id)
+            this.close()
+            this.showCot = c
+            if (this.showCot) {
+                this.showCot.style.display = "block"
+            }
+        },
+        close: function () {
+            if (WebEdit.showCot) {
+                WebEdit.showCot.style.display = "none"
+                WebEdit.showCot = null
+            }
+        },
+        outClose: function (ev, id) {
+            ev = ev || window.event
+            let d = ev.target || ev.srcElement
+            if (d.getAttribute("id") == id) {
+                WebEdit.close()
+            }
+        },
+        format: function (type, para) {
+            this.iframe.focus()
+            if (!para) {
+                this.iframe.document.execCommand(type, false, false)
+            } else {
+                this.iframe.document.execCommand(type, false, para)
+            }
+            this.close()
+        },
+        // 临时
+        createLink: function () {
+            let sURL = window.prompt("请输入网站地址:", "http://")
+            if (sURL != null && sURL != "http://") {
+                this.format("CreateLink", sURL)
+            }
+        },
+        createImg: function () {
+            let sPhoto = prompt("请输入图片位置:", "http://")
+            if (sPhoto != null && sPhoto != "http://") {
+                this.format("InsertImage", sPhoto)
+            }
+        },
+        setHTML: function (str) {
+            this.iframe.document.body.innerHTML = str
+        },
+        //去除HTML
+        getHTML: function () {
+            return this.iframe.document.body.innerHTML
+        },
+        getText: function () {
+            return this.iframe.document.body.textContent || this.iframe.document.body.innerText
+        },
+        clear: function () {
+            this.iframe.document.body.innerHTML = ""
+            return this
+        }
+    })
+
+    WebEdit.htmlDeploys = {
+        fontsize: ['<a v-click="show,fontsize" id="WebEdit.Deploy.fontsize" hidefocus="true" title="字号" href="javascript:;" class="WebEdit_abtn WebEdit_a4"></a>', '<div id="WebEdit.Down.fontsize" class="WebEdit_down" v-mouseleave="close">', '<A v-click="format,fontsize,1" href="javascript:;" style="line-height: 120%; font-size: xx-small;">极小</A>', '<A v-click="format,fontsize,2" href="javascript:;" style="line-height: 120%; font-size: x-small;">特小</A>', '<A v-click="format,fontsize,3" href="javascript:;" style="line-height: 120%; font-size: small;">小</A>', '<A v-click="format,fontsize,4" href="javascript:;" style="line-height: 120%; font-size: medium;">中</A>', '<A v-click="format,fontsize,5" href="javascript:;" style="line-height: 120%; font-size: large;">大</A>', '<A v-click="format,fontsize,6" href="javascript:;" style="line-height: 120%; font-size: x-large;">特大</A>', '<A v-click="format,fontsize,7" href="javascript:;" style="line-height: 120%; font-size: xx-large;">极大</A>', "</div>"].join(""),
+        Bold: '<a v-click="format,Bold,1" v-mouseover="show,Bold" id="WebEdit.Deploy.Bold" hidefocus="true" title="加粗" href="javascript:;" class="WebEdit_abtn WebEdit_a5"></a>',
+        Italic: '<a v-click="format,Italic,1" v-mouseover="show,Italic" id="WebEdit.Deploy.Italic" hidefocus="true" title="斜体" href="javascript:;" class="WebEdit_abtn WebEdit_a6"></a>',
+        Underline: '<a v-click="format,Underline,1" v-mouseover="show,Underline" id="WebEdit.Deploy.Underline" hidefocus="true" title="下划线" href="javascript:;" class="WebEdit_abtn WebEdit_a7"></a>',
+        Justifyleft: '<a v-click="format,Justifyleft,1" v-mouseover="show,Justifyleft" id="WebEdit.Deploy.Justifyleft" hidefocus="true" title="左对齐" href="javascript:;" class="WebEdit_abtn WebEdit_a8"></a>',
+        Justifycenter: '<a v-click="format,Justifycenter,1" ov-mouseover="show,Justifycenter" id="WebEdit.Deploy.Justifycenter" hidefocus="true" title="居中对齐" href="javascript:;" class="WebEdit_abtn WebEdit_a9"></a>',
+        Justifyright: '<a v-click="format,Justifyright,1" v-mouseover="show,Justifyright" id="WebEdit.Deploy.Justifyright" hidefocus="true" title="右对齐" href="javascript:;" class="WebEdit_abtn WebEdit_a10"></a>',
+        Insertorderedlist: '<a v-click="format,Insertorderedlist,1" v-mouseover="show,Insertorderedlist" id="WebEdit.Deploy.Insertorderedlist" hidefocus="true" title="数字编号" href="javascript:;" class="WebEdit_abtn WebEdit_a11"></a>',
+        Insertunorderedlist: '<a v-click="format,Insertunorderedlist,1" v-mouseover="show,Insertunorderedlist" id="WebEdit.Deploy.Insertunorderedlist" hidefocus="true" title="项目编号" href="javascript:;" class="WebEdit_abtn WebEdit_a12"></a>',
+        Indent: '<a v-click="format,Indent,1" v-mouseover="show,Indent" id="WebEdit.Deploy.Outdent" hidefocus="true" title="增加缩进" href="javascript:;" class="WebEdit_abtn WebEdit_a13"></a>',
+        Outdent: '<a v-click="format,Outdent,1" v-mouseover="show,Outdent" id="WebEdit.Deploy.Indent" hidefocus="true" title="减少缩进" href="javascript:;" class="WebEdit_abtn WebEdit_a14"></a>',
+        foreColor: ['<a v-click="show,foreColor" id="WebEdit.Deploy.foreColor" hidefocus="true" title="字体颜色" href="javascript:;" class="WebEdit_abtn WebEdit_a15"></a>', '<div id="WebEdit.Down.foreColor" class="WebEdit_down WebEdit_Color" v-mouseleave="close">', getColors("foreColor"), "</div>"].join(""),
+        backColor: ['<a v-click="show,backColor" id="WebEdit.Deploy.backColor" hidefocus="true" title="背景颜色" href="javascript:;" class="WebEdit_abtn WebEdit_a16"></a>', '<div id="WebEdit.Down.backColor" class="WebEdit_down WebEdit_Color" v-mouseleave="close">', getColors("backColor"), "</div>"].join(""),
+        CreateLink: ['<a v-click="createLink" v-mouseover="show,CreateLink" id="WebEdit.Deploy.CreateLink" hidefocus="true" title="增加连接" href="javascript:;" class="WebEdit_abtn WebEdit_a17"></a>'].join(""),
+        CreateImg: ['<a v-click="createImg" v-mouseover="show,CreateImg" id="WebEdit.Deploy.CreateImg" hidefocus="true" title="增加图片" href="javascript:;" class="WebEdit_abtn WebEdit_a18"></a>'].join(""),
+        "-": ['</td></tr><tr><td nowrap="nowrap">'].join("")
+    }
+})()
